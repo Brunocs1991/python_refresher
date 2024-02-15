7ccee42f520144f98c24528c62a3a3400ba6318a
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-15 12:02:12 -0300

    User Input

---
+++ UserInput.py
@@ -0,0 +1,7 @@
+"""
+User Input
+"""
+
+frist_name = input("Enter your first name: ")
+days = input("How many days before your brithday: ")
+print(f"Hi {frist_name}, only {days} days before your birthday!")
