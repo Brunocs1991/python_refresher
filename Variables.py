f165f726c4751b846d966b8919405c005d12fe6a
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-15 11:44:03 -0300

    .gitignore and frist code Variables into python

---
+++ Variables.py
@@ -0,0 +1,14 @@
+"""
+Variables
+"""
+
+cost = 10
+tax_percent = .25
+tax = cost * tax_percent
+price = cost + tax
+
+print(price)
+
+username = "Codingwithroby"
+frist_name = "Eric"
+print(username + " " + frist_name)
