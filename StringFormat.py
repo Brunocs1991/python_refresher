4c731e6794c0c7a0dd44774e772abe1a6295eef4
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-15 11:57:45 -0300

    String Format

---
+++ StringFormat.py
@@ -0,0 +1,10 @@
+"""
+String Formatting
+"""
+
+frist_name = "Erick"
+last_name = "Roby"
+setence = "Hi {} {}"
+
+print(setence.format(frist_name, last_name))
+print(f"Hi {frist_name} {last_name} I hope you're learning")
