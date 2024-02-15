3af85a86b997e0b1846cfb0604d1a393fabd91b9
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-15 11:48:01 -0300

    second code comments

---
+++ Comments.py
@@ -0,0 +1,19 @@
+# Going to print hello world!
+# print ("Hello World")
+print("Hi Bruno")
+
+# This is going over
+# Multiple
+# Lines
+
+"""
+this is going over
+multiple
+line
+"""
+
+'''
+this is going over 
+multiple
+line
+'''
